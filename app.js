ec8aae0bb1ee9ffdc11253d26b234aada4e583d5
Alabhya Jindal <alabhya10@gmail.com>
2022-03-26 18:42:31 +0530

    chessboard

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Looping a triangle
+
 // for (let i = 1; i <= 7; i++) {
 //   let currentLine = "";
 //   for (let j = 0; j < i; j++) {
@@ -6,14 +8,43 @@
 //   console.log(currentLine);
 // }
 
-for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
+// FizzBuzz
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// Chessboard
+let size = 6;
+
+for (let i = 0; i < size; i++) {
+  if (i % 2 === 0) {
+    let currentLine = "";
+    for (let j = 0; j < size; j++) {
+      if (j % 2 === 0) {
+        currentLine += " ";
+      } else {
+        currentLine += "#";
+      }
+    }
+    console.log(currentLine);
   } else {
-    console.log(i);
+    let currentLine = "";
+    for (let j = 0; j < size; j++) {
+      if (j % 2 === 0) {
+        currentLine += "#";
+      } else {
+        currentLine += " ";
+      }
+    }
+    console.log(currentLine);
   }
 }
