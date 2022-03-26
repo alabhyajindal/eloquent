4a8b57a93ebf01732ccc8ec7248a86703038f837
Alabhya Jindal <alabhya10@gmail.com>
2022-03-26 16:39:14 +0530

    fizzbuzz

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-for (let i = 1; i <= 7; i++) {
-  let currentLine = "";
-  for (let j = 0; j < i; j++) {
-    currentLine += "#";
+// for (let i = 1; i <= 7; i++) {
+//   let currentLine = "";
+//   for (let j = 0; j < i; j++) {
+//     currentLine += "#";
+//   }
+//   console.log(currentLine);
+// }
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
   }
-  console.log(currentLine);
 }
