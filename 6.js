36f5f2acbc1dd7ac91bcf019886f3b93ae129094
Alabhya Jindal <alabhya10@gmail.com>
2022-04-29 15:23:49 +0530

    Completed reading Chapter 6

--- 6.js
+++ 6.js
@@ -249,3 +249,88 @@ class Temperature {
 // let addedVal = foo.plus(new Vec(2, 3));
 // console.log(foo);
 // console.log(addedVal);
+
+// let okIterator = 'OK'[Symbol.iterator]();
+// console.log(okIterator.next());
+// console.log(okIterator.next());
+// console.log(okIterator.next());
+
+// let matrix = new Matrix(2, 3, (a, b) => `${a}, ${b}`);
+// console.log(matrix);
+
+class Matrix {
+  constructor(width, height, element = (x, y) => undefined) {
+    this.width = width;
+    this.height = height;
+    this.content = [];
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        this.content[y * width + x] = element(x, y);
+      }
+    }
+  }
+  get(x, y) {
+    return this.content[y * this.width + x];
+  }
+  set(x, y, value) {
+    this.content[y * this.width + x] = value;
+  }
+}
+
+class MatrixIterator {
+  constructor(matrix) {
+    this.x = 0;
+    this.y = 0;
+    this.matrix = matrix;
+  }
+
+  next() {
+    if (this.y == this.matrix.height) return { done: true };
+
+    let value = {
+      x: this.x,
+      y: this.y,
+      value: this.matrix.get(this.x, this.y),
+    };
+
+    this.x++;
+    if (this.x == this.matrix.width) {
+      this.x = 0;
+      this.y++;
+    }
+    return { value, done: false };
+  }
+}
+
+Matrix.prototype[Symbol.iterator] = function () {
+  return new MatrixIterator(this);
+};
+
+// let matrix = new Matrix(2, 2, (x, y) => `value ${x}, ${y}`);
+// for (let { x, y, value } of matrix) {
+//   console.log(x, y, value);
+// }
+
+class SymmetricMatrix extends Matrix {
+  constructor(size, element = (x, y) => undefined) {
+    super(size, size, (x, y) => {
+      if (x < y) return element(y, x);
+      else return element(x, y);
+    });
+  }
+  set(x, y, value) {
+    super.set(x, y, value);
+    if (x != y) {
+      super.set(y, x, value);
+    }
+  }
+}
+
+let matrix = new SymmetricMatrix(5, (x, y) => `${x}, ${y}`);
+// console.log(matrix.get(2, 3));
+
+console.log(new SymmetricMatrix(2) instanceof SymmetricMatrix);
+console.log(new SymmetricMatrix(2) instanceof Matrix);
+console.log(new Matrix(2, 2) instanceof SymmetricMatrix);
+console.log([1] instanceof Array);
