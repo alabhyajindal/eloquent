ff85cdd86f7ea35e242f08d3da54c98299156467
Alabhya Jindal <alabhya10@gmail.com>
2022-06-06 16:51:13 +0530

    Retry

--- 8.js
+++ 8.js
@@ -1,49 +1,49 @@
-function test(label, body) {
-  if (!body) return `Failed ${label}`;
-}
+// function test(label, body) {
+//   if (!body) return `Failed ${label}`;
+// }
 
-test('convert Latin text to uppercase', () => {
-  return 'hello'.toUpperCase === 'HELLO';
-});
+// test('convert Latin text to uppercase', () => {
+//   return 'hello'.toUpperCase === 'HELLO';
+// });
 
-test('convert Greek text to uppercase', () => {
-  return 'Χαίρετε'.toUpperCase === 'ΧΑΊΡΕΤΕ';
-});
+// test('convert Greek text to uppercase', () => {
+//   return 'Χαίρετε'.toUpperCase === 'ΧΑΊΡΕΤΕ';
+// });
 
-test("don't convert case-less characters", () => {
-  return 'سلام'.toUpperCase === 'سلام';
-});
+// test("don't convert case-less characters", () => {
+//   return 'سلام'.toUpperCase === 'سلام';
+// });
 
-function promptDirection(question) {
-  let result = prompt(question);
-  if (result.toLowerCase() == 'left') return 'L';
-  if (result.toLowerCase() == 'right') return 'R';
-  throw new Error(`Invalid direction: ${result}`);
-}
+// function promptDirection(question) {
+//   let result = prompt(question);
+//   if (result.toLowerCase() == 'left') return 'L';
+//   if (result.toLowerCase() == 'right') return 'R';
+//   throw new Error(`Invalid direction: ${result}`);
+// }
 
-function look() {
-  if (promptDirection('Which way') == 'L') {
-    return 'a house';
-  } else {
-    return 'two angry bears';
-  }
-}
+// function look() {
+//   if (promptDirection('Which way') == 'L') {
+//     return 'a house';
+//   } else {
+//     return 'two angry bears';
+//   }
+// }
 
-try {
-  console.log('You see', look());
-} catch (e) {
-  console.error(`Something went wrong: ${e}`);
-}
+// try {
+//   console.log('You see', look());
+// } catch (e) {
+//   console.error(`Something went wrong: ${e}`);
+// }
 
-const accounts = { a: 100, b: 0, c: 20 };
+// const accounts = { a: 100, b: 0, c: 20 };
 
-function getAccount() {
-  let accountName = prompt('Enter an account name');
-  if (!accounts.hasOwnProperty(accountName)) {
-    throw new Error(`No such account: ${accountName}`);
-  }
-  return accountName;
-}
+// function getAccount() {
+//   let accountName = prompt('Enter an account name');
+//   if (!accounts.hasOwnProperty(accountName)) {
+//     throw new Error(`No such account: ${accountName}`);
+//   }
+//   return accountName;
+// }
 
 // function transfer(from, amount) {
 //   if (accounts[from] < amount) return;
@@ -51,17 +51,47 @@ function getAccount() {
 //   accounts[getAccount()] += amount;
 // }
 
-function transfer(from, amount) {
-  if (accounts[from] < amount) return;
-  let progress = 0;
-  try {
-    accounts[from] -= amount;
-    progress = 1;
-    accounts[getAccount()] += amount;
-    progress = 2;
-  } finally {
-    if (progress == 1) {
-      accounts[from] += amount;
+// function transfer(from, amount) {
+//   if (accounts[from] < amount) return;
+//   let progress = 0;
+//   try {
+//     accounts[from] -= amount;
+//     progress = 1;
+//     accounts[getAccount()] += amount;
+//     progress = 2;
+//   } finally {
+//     if (progress == 1) {
+//       accounts[from] += amount;
+//     }
+//   }
+// }
+
+// Retry
+
+class MultiplicatorUnitFailure extends Error {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure('Klunk');
+  }
+}
+
+function reliableMultiply(a, b) {
+  // Your code here.
+  for (;;) {
+    try {
+      return primitiveMultiply(a, b);
+    } catch (e) {
+      if (e instanceof MultiplicatorUnitFailure) {
+        console.log('Trying again');
+      } else {
+        throw e;
+      }
     }
   }
 }
+
+console.log(reliableMultiply(8, 8));
+// → 64
