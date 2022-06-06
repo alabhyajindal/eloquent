2b7649f8f01d71ae52fb6250e65b170f152a624a
Alabhya Jindal <alabhya10@gmail.com>
2022-06-06 18:03:55 +0530

    The Locked Box

--- 8.js
+++ 8.js
@@ -68,30 +68,71 @@
 
 // Retry
 
-class MultiplicatorUnitFailure extends Error {}
+// class MultiplicatorUnitFailure extends Error {}
 
-function primitiveMultiply(a, b) {
-  if (Math.random() < 0.2) {
-    return a * b;
-  } else {
-    throw new MultiplicatorUnitFailure('Klunk');
-  }
-}
+// function primitiveMultiply(a, b) {
+//   if (Math.random() < 0.2) {
+//     return a * b;
+//   } else {
+//     throw new MultiplicatorUnitFailure('Klunk');
+//   }
+// }
 
-function reliableMultiply(a, b) {
+// function reliableMultiply(a, b) {
+//   // Your code here.
+//   for (;;) {
+//     try {
+//       return primitiveMultiply(a, b);
+//     } catch (e) {
+//       if (e instanceof MultiplicatorUnitFailure) {
+//         console.log('Trying again');
+//       } else {
+//         throw e;
+//       }
+//     }
+//   }
+// }
+
+// console.log(reliableMultiply(8, 8));
+
+// The Locked Box
+
+const box = {
+  locked: true,
+  unlock() {
+    this.locked = false;
+  },
+  lock() {
+    this.locked = true;
+  },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error('Locked!');
+    return this._content;
+  },
+};
+
+function withBoxUnlocked(body) {
   // Your code here.
-  for (;;) {
-    try {
-      return primitiveMultiply(a, b);
-    } catch (e) {
-      if (e instanceof MultiplicatorUnitFailure) {
-        console.log('Trying again');
-      } else {
-        throw e;
-      }
-    }
+  let isLocked = box.locked;
+  try {
+    if (isLocked) box.unlock();
+    return body();
+  } finally {
+    if (isLocked) box.lock();
   }
 }
 
-console.log(reliableMultiply(8, 8));
-// → 64
+withBoxUnlocked(function () {
+  box.content.push('gold piece');
+});
+
+try {
+  withBoxUnlocked(function () {
+    throw new Error('Pirates on the horizon! Abort!');
+  });
+} catch (e) {
+  console.log('Error raised: ' + e);
+}
+console.log(box.locked);
+// → true
