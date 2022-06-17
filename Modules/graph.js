2718f699c77fe48056f394eb1e8f0d4be43b33cc
Alabhya Jindal <alabhya10@gmail.com>
2022-06-17 11:12:20 +0530

    Roads Module

---
+++ Modules/graph.js
@@ -0,0 +1,15 @@
+module.exports = buildGraph;
+
+function buildGraph(roads) {
+  let graph = {};
+  let edges = [];
+  for (let i = 0; i < roads.length; i++) {
+    let currentPair = roads[i].split('-');
+    let [from, to] = currentPair;
+    if (graph[from] == undefined) graph[from] = [to];
+    else graph[from].push(to);
+    if (graph[to] == undefined) graph[to] = [from];
+    else graph[to].push(to);
+  }
+  return graph;
+}
