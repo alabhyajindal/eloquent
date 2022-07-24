00ed8b089f8fdbc4f8e1f9860b24d222812e6877
Alabhya Jindal <alabhya10@gmail.com>
2022-07-24 22:56:30 +0530

    Tried out the API

--- Node.js/main.js
+++ Node.js/main.js
@@ -28,29 +28,28 @@
 // const { readFileSync } = require('fs');
 // console.log(readFileSync('file.txt', 'utf-8'));
 
-// const { createServer } = require('http');
-// let server = createServer((request, response) => {
-//   response.writeHead(200, { 'Content-Type': 'text/html' });
-//   response.write(`<h1>Hello</h1>
-//   <p>You asked for <code>${request.url}</code></p>
-//   `);
-//   response.end();
-// });
+const { createServer } = require('http');
+let server = createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(`<h1>Hello World</h1>
+  `);
+  response.end();
+});
 
-// server.listen(8000);
-// console.log('Listening at port 8000');
-// console.log('http://localhost:8000');
+server.listen(8000);
+console.log('Listening at port 8000');
+console.log('http://localhost:8000');
 
-const { request } = require('http');
-let requestStream = request(
-  {
-    hostname: 'eloquentjavascript.net',
-    path: '/20_node.html',
-    method: 'GET',
-    headers: { Accept: 'text/html' },
-  },
-  (response) => {
-    console.log('Server responded with status code', response.statusCode);
-  }
-);
-requestStream.end();
+// const { request } = require('http');
+// let requestStream = request(
+//   {
+//     hostname: 'eloquentjavascript.net',
+//     path: '/20_node.html',
+//     method: 'GET',
+//     headers: { Accept: 'text/html' },
+//   },
+//   (response) => {
+//     console.log('Server responded with status code', response.statusCode);
+//   }
+// );
+// requestStream.end();
