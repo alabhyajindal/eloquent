2648ab7cd4b79d690f2f0a695ff8598eaede2751
Alabhya Jindal <alabhya10@gmail.com>
2022-07-24 22:37:17 +0530

    The HTTP Module

--- Node.js/main.js
+++ Node.js/main.js
@@ -25,5 +25,32 @@
 // const { readFile } = require('fs').promises;
 // readFile('file.txt', 'utf-8').then((text) => console.log(text));
 
-const { readFileSync } = require('fs');
-console.log(readFileSync('file.txt', 'utf-8'));
+// const { readFileSync } = require('fs');
+// console.log(readFileSync('file.txt', 'utf-8'));
+
+// const { createServer } = require('http');
+// let server = createServer((request, response) => {
+//   response.writeHead(200, { 'Content-Type': 'text/html' });
+//   response.write(`<h1>Hello</h1>
+//   <p>You asked for <code>${request.url}</code></p>
+//   `);
+//   response.end();
+// });
+
+// server.listen(8000);
+// console.log('Listening at port 8000');
+// console.log('http://localhost:8000');
+
+const { request } = require('http');
+let requestStream = request(
+  {
+    hostname: 'eloquentjavascript.net',
+    path: '/20_node.html',
+    method: 'GET',
+    headers: { Accept: 'text/html' },
+  },
+  (response) => {
+    console.log('Server responded with status code', response.statusCode);
+  }
+);
+requestStream.end();
